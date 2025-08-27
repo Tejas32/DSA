979629511009a26d6eeb2220c3e5725123ecb81e
Tejas Mundhe <tdmundhe32@gmail.com>
2025-08-27 18:51:38 +0530

    Initial Commit

---
+++ 1_Basics/addition.java
@@ -0,0 +1,13 @@
+public class addition {
+
+    public static int sum(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int addition = sum(a, b);
+        System.out.println(addition);
+    }
+}
